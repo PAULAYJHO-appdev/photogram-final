f97eb1deea5b0132a50588e8076bec7a0784f1eb
Paula Ho <pho2@chicagobooth.edu>
2023-05-19 00:48:59 +0000

    Created User Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Homepage
+  get("/", { :controller => "users", :action => "index" })
+
+  #Users
+
+
+  #Photos
+
+
+  #Sign In
+
+
+  #Sign Up
 end

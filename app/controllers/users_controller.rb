f3dd07a0c9802c966b2a6100600848eddaff6fcd
Paula Ho <pho2@chicagobooth.edu>
2023-05-20 17:09:10 +0000

    Added Table on "/" "/users" page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.order({ :username => :asc })
     render({ :template => "user_templates/index.html.erb"})
-  end
-
-  def authenticate
-    render({ :template => "user_templates/signin_form.html.erb"})
-  end
-
-  def new_registration_form
-    render({ :template => "user_templates/signup_form.html.erb"})
   end
 end
